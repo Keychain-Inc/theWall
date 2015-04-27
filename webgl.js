6169cca00a06c160d355d3a4c0c549334fcb3fc5
tobimo <tobiasml@Tobias-MacBook.local>
2015-04-27 22:10:00 +0200

    Simple seashell creator web app
    Basic HTML page with inputs to manipulate the WebGL canvas, which
    renders a seashell.

---
+++ webgl.js
@@ -0,0 +1,122 @@
+var renderer = new THREE.WebGLRenderer();
+
+
+function computeShellGeometry() {
+	var geometry = new THREE.Geometry();
+	
+	// Auxiliary function to push a Cartesian coordinate onto the three.js geometry
+	function pushCart(c) {
+		geometry.vertices.push( new THREE.Vector3(c.x, c.y, c.z) )
+	}
+	
+	
+	//TODO: Add a Bool "dirty" when changing the UI, and call this whenever the bool is dirty
+	//Look into using the three.js variable MeshNeedsUpdating
+	var c = shell.getCartCoords()
+
+	// Push the seashell coordinates onto the geometry's vertex array
+	for (i in c) {
+		for (j in c[i] ){
+			pushCart(c[i][j])
+		}
+	}
+
+	// Define faces using the pushed vertices
+	for (i = 0; i < geometry.vertices.length - (shell.bezres+1) ; i++) { // we look ahead in the index, so we stop i before the end
+		geometry.faces.push( new THREE.Face3( i+1, i, i+shell.bezres ) );	
+		geometry.faces.push( new THREE.Face3( i+1, i+shell.bezres, i+shell.bezres+1 ) );
+	}
+	return geometry;
+}
+
+var scene = new THREE.Scene();
+
+// Create a material for our mesh
+var material = new THREE.MeshPhongMaterial( { color: 0xffffff, shininess: 0, shading: THREE.FlatShading, side:THREE.DoubleSide } )
+
+// Create the mesh, add it to the scene
+var shellMesh = new THREE.Mesh( computeShellGeometry(), material );
+shellMesh.rotation.y -=3.14
+scene.add(shellMesh);
+
+// Lights...
+var directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
+directionalLight.position.set( 10, 10, 10 );
+scene.add( directionalLight );
+
+var light = new THREE.AmbientLight( 0x101010 );
+scene.add( light );
+
+//...camera...
+var camera = new THREE.PerspectiveCamera( 75, 1, 0.001, 1000 );
+camera.position.z = 15;
+
+//...action!
+function render() {
+	if (dirty) {
+		scene = new THREE.Scene();
+		
+		// Create the mesh, add it to the scene
+		shellMesh = new THREE.Mesh( computeShellGeometry(), material );
+		shellMesh.rotation.y -=3.14
+		scene.add(shellMesh);
+		scene.add( directionalLight );
+		
+		dirty = false;
+	}
+	renderer.render(scene, camera );
+}
+
+render();
+
+//Periodically refresh
+//I wanted to use window.requestAnimationFrame but my browser does not support it
+window.setInterval("render()", 1000/30)
+
+// Simple functions for rotating the geometry
+// Code taken from https://stackoverflow.com/questions/17015019/keylistener-in-javascript :
+document.onkeydown = function (e) {
+	var code = e.keyCode ? e.keyCode : e.which;
+	if (code === 37) { //left key
+		shellMesh.rotation.y -=0.1
+		return false;
+	} else if (code === 38) { //up key
+		shellMesh.rotation.x -=0.1
+		return false;
+	} else if (code === 39) { //right key
+		shellMesh.rotation.y +=0.1
+		return false;
+	} else if (code === 40) { //down key
+		shellMesh.rotation.x +=0.1
+		return false;
+	} else if (code === 107) { //plus key
+		camera.position.z -= 0.1
+		return false;
+	} else if (code === 109) { //minus key
+		camera.position.z += 0.1
+		return false;
+	}
+};
+
+function setupGraphics() {
+	
+	
+	//Get our display element:
+	var container = document.getElementById( "graphicsDiv" )
+	var width = container.offsetWidth;
+	var height = container.offsetHeight;
+	
+	renderer.setSize(width, height);
+	camera.aspect=(width/height)
+	camera.updateProjectionMatrix()
+	
+	container.appendChild(renderer.domElement);
+	
+	// Sync UI to the camera
+	document.getElementById("camera").value = camera.position.z
+	
+	// Add event listeners:
+	document.getElementById("camera").oninput = function() { camera.position.z = this.value; }
+	
+	
+}
