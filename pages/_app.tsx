77f812fe8ee25920908da9cbad67c62ddb2d8e6c
pr0toshi <pr0@tama.lol>
2022-10-08 12:41:07 +0100

    tt

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
 import '@rainbow-me/rainbowkit/styles.css'
-import { useWeb3React } from "@web3-react/core";
+import {useWeb3React} from "@web3-react/core";
 import { useEffect, useState } from "react";
 import abi from '../contracts/theWall.json'
-
+import Head from 'next/head'
+import Image from 'next/image'
+import { Toaster } from 'react-hot-toast'
+import AdminPanel from '../components/adminPanel'
+import Navbar from '../components/navbar'
 
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
@@ -15,7 +19,7 @@ import { alchemyProvider } from 'wagmi/providers/alchemy'
 import { publicProvider } from 'wagmi/providers/public'
 
 import config from '../config/env-vars'
-import { BigNumber, ethers } from 'ethers'
+import { BigNumber, ethers} from 'ethers'
 import { RainbowKitChainProvider } from '@rainbow-me/rainbowkit/dist/components/RainbowKitProvider/RainbowKitChainContext'
 import { Provider } from 'web3/providers'
 import Home from '.';
@@ -47,7 +51,6 @@ const wagmiClient = createClient({
 // send ether and pay to change state within the blockchain.
 // For this, you need the account signer...
 const signerw = wagmiClient.provider;
-const signer = wagmiClient.connector?.getSigner;
 
 const contractaddrs = "0x91fc82f5c588c00985aa264fc7b45ee680110703";
 
@@ -70,28 +73,26 @@ function format_address(address) {
   return new_address;
 }
 
-
 function tag0() {
   const [tag, setTag] = useState("LOL");
   const [artist, setArtist] = useState("LOL");
   const [time, setTime] = useState("");
   const [sup, setSup] = useState(2);
   // effect hook for updating data
+  
   useEffect(() => {
 
     // update the ui elements
     async function updateUIStates() {
-
       const [tagS, artistS, timeS] = await Contract.latest(await Contract.totalSupply());
       // set balance
       setTag(tagS);
       setArtist(artistS);
       setTime(timeS);
       let s = (await Contract.totalSupply());
-      s = ethers.utils.formatUnits(s, 0);
+      s = ethers.utils.formatUnits(s,0);
       setSup(s)
-
-    };
+      };
     // fix for updatix1ng after wallet login
     updateUIStates();
 
@@ -105,7 +106,7 @@ function tag0() {
     return () => clearInterval(timer);
   },);
 
-
+  
   function tag1() {
     let tags = []
     let artists = []
@@ -117,23 +118,21 @@ function tag0() {
       tags[n] = tag[n]
       artists[n] = artist[n]
       times[n] = time[n]
-      //ts[n] = ethers.utils.formatUnits(time[0]);
-      // times[n] = ethers.utils.formatUnits(ts[n],0);
-
-      if (tags[n] != '') {
-        t0[n] = (<div>
-          <h2 className="text-1xl text-center font-bold justify-center light:text-gray-800">
-            From {artists[n]} #{sup - n}
-          </h2>
-          <div className="text-center light:text-white-600">
-            {tags[n]}
-          </div>
-          <div className="text-center light:text-white-600">
-            _____________________________________________________________________________________________
-          </div>
-        </div>
-        )
-      }
+    //ts[n] = ethers.utils.formatUnits(time[0]);
+     // times[n] = ethers.utils.formatUnits(ts[n],0);<p style={{color: 'blue'}}>
+      if (tags[n] != ''){
+      t0[n] = (<div >
+        <h2 className="text-1xl text-center font-bold justify-center light:text-gray-800 ">
+        <a style={{color: '#4f86f7'}}> # {sup-n}</a>  From   <a className="rotating-hue" style={{color: '#4f86f7'}}>{artists[n]}</a>
+        </h2>
+        <div className="text-center light:text-white-600" >
+          {tags[n]}
+           </div>
+           <div className="text-center light:text-white-600"><a style={{color: '#32353B'}}>
+           _____________________________________________________________________________________________
+                      </a></div>
+      </div>
+      )}
     }
     let t1 = [<div className="text-1xl font-bold light:text-gray-800">Messages: {sup}</div>]
     return (
@@ -153,50 +152,52 @@ const App = ({ Component, pageProps }: AppProps) => {
     const values = event.target.value;
     setSendMessage(values);
   }
-  //notify
-  const [sendMessage, setSendMessage] = useState("TT");
-  const [unlocktext, set_unlocktext] = useState("Please Unlock Wallet");
+   //notify
+   const [sendMessage, setSendMessage] = useState("TT");
+   const [unlocktext, set_unlocktext] = useState("Please Unlock Wallet");
   // notify function call
   const callTag = async () => {
     try {
       wagmiClient.autoConnect();
-      await Contract.mint(Contract.address, sendMessage);
+      await Contract.mint(Contract.address, sendMessage, { value: ethers.utils.parseUnits(".1", "ether") });
+      setSendMessage("2");
     } catch (e) {
       console.log("LOL")
-      // addToast({body: e.message, type: "error"});
+       // addToast({body: e.message, type: "error"});
     }
-    //const{isLoading,error, write} = useContractWrite({
-    //  addressOrName:"0x91fc82f5c588c00985aa264fc7b45ee680110703",
-    //  contractInterface:abi,
-    //});
-
+//const{isLoading,error, write} = useContractWrite({
+//  addressOrName:"0x91fc82f5c588c00985aa264fc7b45ee680110703",
+//  contractInterface:abi,
+//});
+    
   };
   return (
+    
     <ThemeProvider attribute="class">
       <div className="m-auto bg-white dark:bg-gray-900 dark:text-white">
         <WagmiConfig client={wagmiClient}>
           <RainbowKitProvider chains={chains}>
             <Component {...pageProps} />
-            <div className="flex flex-col space-y-2 justify-center mt-6 md:mt-12 px-4 xs:px-0 m-auto max-w-4xl min-w-80 shadow-md rounded-md border border-solid light:border-gray-200 dark:border-gray-500 overflow-hidden">
-              <h1 className="m-auto text-center md:mt-8 text-2xl md:text-4xl font-extrabold rotating-hue">
+            <div className="flex flex-col space-y-2 justify-center mt-6 md:mt-2 px-4 xs:px-0 m-auto max-w-4xl min-w-80 shadow-md rounded-md border border-solid light:border-gray-200 dark:border-gray-500 overflow-hidden">
+            <h1 className="m-auto text-center md:mt-8 text-2xl md:text-4xl font-extrabold rotating-hue">
                 Tag the Wall! {sendMessage}
               </h1>
               <h2 className="text-1xl text-center font-bold justify-center light:text-gray-800">
-                Send your message here
-              </h2>
+        Send your message here
+        </h2>
 
-              <textarea className="m-auto text-center w-3/4 justify-center rounded-md border border-solid light:border-gray-200 dark:border-gray-500 "
-                onChange={e => handleChangeMessage(e)} />
-
-              <button style={{ background: "#00ffff" }} className="btn w-6/12 m-auto " type="button"
-                onClick={callTag}> Send
-              </button>
+        <textarea className="m-auto text-center w-3/4 justify-center rounded-md border border-solid light:border-gray-200 dark:border-gray-500 "
+                                      onChange={e => handleChangeMessage(e)}/>
+          
+                    <button style={{background: "#00ffff"}} className="btn w-6/12 m-auto rounded-md border border-solid light:border-black dark:border-black" type="button"
+                            onClick={callTag}> Send 
+                            </button>
+                            <div>
+              
+              </div>  
               <div>
-
-              </div>
-              <div>
-
-              </div>
+              
+              </div>  
             </div>
             <div className="flex flex-col space-y-6 justify-center mt-6 md:mt-12 px-4 xs:px-0 m-auto max-w-4xl min-w-80 shadow-md rounded-md border border-solid light:border-gray-200 dark:border-gray-500 overflow-hidden">
 
@@ -204,10 +205,10 @@ const App = ({ Component, pageProps }: AppProps) => {
                 Chat
               </h1>
               <div>
-
-                {tag0()}
-              </div>
-
+                
+                  {tag0()}
+                </div>
+                
             </div>
           </RainbowKitProvider>
         </WagmiConfig>
