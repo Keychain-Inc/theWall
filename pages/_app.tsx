8ded27879bb1f1c8f451ecc4e6ab4c300d9e4350
pr0toshi <pr0@tama.lol>
2022-10-10 20:33:32 +0100

    tt

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -187,7 +187,7 @@ await provider2.send("eth_requestAccounts", []);
             
             <div className="flex flex-col space-y-2 justify-center mt-6 md:mt-2 px-4 xs:px-0 m-auto max-w-4xl min-w-80 shadow-md rounded-md border border-solid light:border-gray-200 dark:border-gray-500 overflow-hidden">
             <h1 className="m-auto text-center md:mt-8 text-2xl md:text-4xl font-extrabold rotating-hue">
-                Tag the Wall! {sendMessage}
+                Tag the Wall!
               </h1>
               <h2 className="text-1xl text-center font-bold justify-center light:text-gray-800">
         Send your message here
