74517051052559521760202bfce8d0025eb64b90
pr0toshi <pr0@tama.lol>
2022-11-10 15:24:53 +0000

    tt

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -359,7 +359,7 @@ const App = ({ Component, pageProps }: AppProps) => {
     //signerw ethers.js .send("eth_requestAccounts", []);
 
     if ((await provider2.getNetwork()).chainId == 137) {
-      window.location.replace('https://app.uniswap.org/#/swap?inputCurrency=ETH&outputCurrency=0x42b54830bcbb0a240aa54cd3f8d1a4db00851fe3')
+      window.location.replace('https://app.uniswap.org/#/swap?inputCurrency=ETH&outputCurrency=0x7b67595bbfc2c900e1a66a35ab35e762765e062d')
     }
     else {
       toast.error('Polygon Network')
