714c2ce36c8d49c0c3957a67315b4d9bea9c35d3
pr0toshi <46229158+pr0toshi@users.noreply.github.com>
2023-01-25 12:39:47 +0000

    tt

--- config/network.tsx
+++ config/network.tsx
@@ -57,7 +57,7 @@ const BSCA = '0x18C519E0dA619d017908aFf504e782E381552620'
 const BSCN = '0xB8112446078378f0998FBf834D4683B6C8Ac08C7'
 const BSCM = [<MenuItem value={'0x18C519E0dA619d017908aFf504e782E381552620'}>Main</MenuItem>]
 const PolyM = [<MenuItem value={'0x00f0feed50dcdf57b4f1b532e8f5e7f291e0c84b'}>Main</MenuItem>,
-<MenuItem value={'0x3783b8DAf1B14d8e556ec1C681CEB0056157e810'}>Sub Wall</MenuItem>,
+<MenuItem value={'0x79ae22bf1db3938db61989e805e5cd1d5990007c'}>Demo</MenuItem>,
 ]
 const PolyC = chain.polygon
 const PolyRPC = 'https://polygon-rpc.com'
